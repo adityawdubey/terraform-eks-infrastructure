73908cddd1490fb137c5edbe35e6cbfc29817957
adityawdubey <adityawdubey@gmail.com>
2025-04-19 14:54:04 +0530

    Update Terraform backend configuration

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "my-terraform-state-bucket"
-#     key            = "eks/terraform.tfstate"
-#     region         = "ap-south-1"
-#     encrypt        = true
-#     dynamodb_table = "terraform-lock"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-aditya123"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
